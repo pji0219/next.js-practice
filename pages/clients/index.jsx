e9a8b8c8d8879ddef7f2a9dca0403e107b112cd5
pji0219 <pji219@naver.com>
2023-01-03 19:46:20 +0900

    네비게이팅

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+// 동적 라우트로 네티게이팅 하기
+import Link from 'next/link';
 
 function ClientPage() {
+  const clients = [
+    {
+      id: 'ksh',
+      name: '김소현',
+    },
+    {
+      id: 'kyj',
+      name: '김유정',
+    },
+  ];
+
   return (
     <div>
       <h1>클라이언트 페이지!</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 function HomePage() {
-  return <h1>홈 페이지!</h1>;
+  return (
+    <div>
+      <h1>홈 페이지!</h1>;
+      <ul>
+        <li>
+          <Link href='/portfolio'>포트폴리오</Link>
+        </li>
+        <li>
+          <Link href='/clients'>클라이언트</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
