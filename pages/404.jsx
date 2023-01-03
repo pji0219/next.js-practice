76ba78290e31a86cc4909545342d139ca3ada244
pji0219 <pji219@naver.com>
2023-01-03 23:47:10 +0900

    404

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <div>
+      <h1>존재하지 않는 페이지 입니다.</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -7,9 +7,15 @@ function CientProjectPage() {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load data...
+    router.push('/clients/kyj/projecta');
+  };
+
   return (
     <div>
       <h1>해당 클라이언트와의 프로젝트</h1>
+      <button onClick={loadProjectHandler}>프로젝트 A 불러오기</button>
     </div>
   );
 }
